44b6831586492ff755a5396b3818ab17be57d39c
Andreas Gasser <gasserandreas@users.noreply.github.com>
2023-02-19 18:14:22 +0100

    task: changed DNS validation

--- acm-certificate/main.tf
+++ acm-certificate/main.tf
@@ -3,13 +3,13 @@ variable "root_domain_name" {}
 variable "www_domain_name" {}
 
 variable "validation_method" {
-  default = "EMAIL"
+  default = "DNS"
 }
 
 resource "aws_acm_certificate" "hosting_certificate" {
   // We want a wildcard cert so we can host subdomains later.
-  domain_name       = "${var.root_domain_name}"
-  validation_method = "${var.validation_method}"
+  domain_name       = var.root_domain_name
+  validation_method = var.validation_method
 
   // We also want the cert to be valid for the root domain even though we'll be
   // redirecting to the www. domain immediately.
@@ -17,5 +17,5 @@ resource "aws_acm_certificate" "hosting_certificate" {
 }
 
 output "arn_hosting" {
-  value = "${aws_acm_certificate.hosting_certificate.arn}"
+  value = aws_acm_certificate.hosting_certificate.arn
 }
